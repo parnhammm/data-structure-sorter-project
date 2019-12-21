be6124d82e53d0e9cb365317c862e2d7a3c4b140
Andrew Parnham <atparnham@gmail.com>
2019-12-21 10:27:46 +0000

    Implement a linked list of numbers for the sorting project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, -3, 5, 1]);
-const sorter = new Sorter(numbersCollection);
+let sorter = new Sorter(numbersCollection);
 
 sorter.bubbleSort();
 
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("aXaa");
+sorter = new Sorter(charactersCollection);
+
+sorter.bubbleSort();
+
+console.log(charactersCollection.data);
