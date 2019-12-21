c8a0de88ad6362545c7dba5c84ef99c850b79827
Andrew Parnham <atparnham@gmail.com>
2019-12-21 09:25:16 +0000

    Refactoring out logic into own class as first stage of a refactor

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,26 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  /**
+   * Create a getter function to allow access like a property
+   */
+  get length(): number {
+    return this.data.length;
+  }
+
+  public compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  public swap(leftIndex: number, rightIndex: number): void {
+    //We need to swap the values
+    //Take a copt of the element on the left
+    const temp = this.data[leftIndex];
+
+    //The value on the left becomes what was on the right
+    this.data[leftIndex] = this.data[rightIndex];
+
+    //The right value is updated to our copy of the left
+    this.data[rightIndex] = temp;
+  }
+}
--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,5 +1,7 @@
+import { NumbersCollection } from "./NumbersCollection";
+
 export class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: NumbersCollection) {}
 
   /**
    * Attempts to sort the collection using the bubble sort method
@@ -10,16 +12,8 @@ export class Sorter {
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          //We need to swap the values
-          //Take a copt of the element on the left
-          const temp = this.collection[j];
-
-          //The value on the left becomes what was on the right
-          this.collection[j] = this.collection[j + 1];
-
-          //The right value is updated to our copy of the left
-          this.collection[j + 1] = temp;
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
         }
       }
     }
