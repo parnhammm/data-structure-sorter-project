77ec5fd6941b9fbcdbba516015695722305f97ea
Andrew Parnham <atparnham@gmail.com>
2019-12-21 09:34:18 +0000

    Create interface to define how to be sorted.

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,5 +1,13 @@
 import { NumbersCollection } from "./NumbersCollection";
 
+export interface Sortable {
+  length: number;
+
+  swap(leftIndex: number, rightIndex: number): void;
+
+  compare(leftIndex: number, rightIndex: number): boolean;
+}
+
 export class Sorter {
   constructor(public collection: NumbersCollection) {}
 
